7f98fb18ce8a9c2e2dd2b01f1ef9395982034462
Guilherme Melo <guilherme96melo@gmail.com>
2025-02-20 20:06:12 +0100

    Revision del ejercicio para mejorarlo y evitar que el programa se rompa al introducir una variable no entera. Utiliza un metodo para ello isEntero.

--- src/Factorial.java
+++ src/Factorial.java
@@ -4,12 +4,26 @@ public class Factorial {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
         System.out.println("Introduce un número entero: ");
-        int num = lectura.nextInt();
+        String numsString = lectura.nextLine();
+        if (isEntero(numsString) == false) {
+            System.out.println("El valor introducido no es un número entero");
+            System.exit(0);
+            
+        }
+        int num = Integer.parseInt(numsString);
         int factorial = 1;
         for (int i = 1; i <= num; i++){
             factorial *= i;
         }
         System.out.println("El factorial de " + num + " es " + factorial);
         lectura.close();    
+    }
+    public static boolean isEntero(String str){
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e){
+            return false;
+        }
     }
 }
